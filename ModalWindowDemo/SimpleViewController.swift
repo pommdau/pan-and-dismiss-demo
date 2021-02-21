9287118916fd8f0c9bf13864f494bc55550ccf65
Hiroki Ikeuchi <pondau1024@gmail.com>
2021-02-22 00:44:03 +0900

    不要なクラスの削除

--- ModalWindowDemo/SimpleViewController.swift
+++ ModalWindowDemo/SimpleViewController.swift
@@ -33,8 +33,6 @@ class SimpleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        self.view.backgroundColor = UIColor(red: 1.0, green: 0, blue: 0, alpha: 0.2)
-
         configureUI()
     }
     
